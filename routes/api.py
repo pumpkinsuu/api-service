974dbde6c81c5d9e6928b79b0cac8e98b888b341
pumpkinsuu <pumpkinsuu@gmail.com>
2021-05-21 13:03:23 +0700

    update

--- routes/api.py
+++ routes/api.py
@@ -2,26 +2,7 @@ from flask import Blueprint, request
 
 import services.moodle as moodle
 from utilities import ErrorAPI, response
-
-from moodle_config import DEF_ROLE, ADMIN_ROLE
-from config import API_KEY
-
-
-def verify(args, admin=False):
-    if args['token'] == API_KEY:
-        return
-
-    role = DEF_ROLE
-    if admin:
-        role = ADMIN_ROLE
-
-    result = moodle.token_info(args['token'])
-    if not result:
-        raise ErrorAPI(401, 'unauthorized request')
-
-    user = moodle.user_info(result['username'])
-    if not user or user['roleid'] not in role:
-        raise ErrorAPI(401, 'no permission')
+from routes.verify import verify
 
 
 api_bp = Blueprint('api_bp', __name__)
