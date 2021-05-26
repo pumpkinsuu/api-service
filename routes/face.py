d74752339da79a6c8c472923b6439f82daefde49
pumpkinsuu <pumpkinsuu@gmail.com>
2021-05-26 21:43:00 +0700

    update

--- routes/face.py
+++ routes/face.py
@@ -185,7 +185,7 @@ def check(roomID: str):
             if moodle.checkin(roomID, userID, 1):
                 user['status'] = 1
             else:
-                user['status'] = 3
+                user['status'] = 2
 
             data = moodle.get_image(userID)
             if data:
