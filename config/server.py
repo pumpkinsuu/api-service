21837bfb5b0903628a90138f4ced1ee3336608fb
pumpkinsuu <pumpkinsuu@gmail.com>
2021-06-23 14:33:33 +0700

    update

--- config/server.py
+++ config/server.py
@@ -3,7 +3,6 @@ HOST = '0.0.0.0'
 PORT = 5000
 # Face service
 FACE_URL = 'http://localhost:5001'
-SECRET = 'secret_key'
 # Mongo
 MONGO_URI = f"mongodb+srv://admin:AdminPass123" \
             f"@cluster0.zfker.mongodb.net/"
